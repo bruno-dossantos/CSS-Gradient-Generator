dd82b711f27b1de3b0b590ba6482a30208eeb220
bruno-dossantos <contactobrunods@gmail.com>
2022-02-16 23:10:05 -0300

    footer component

---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,30 @@
+import "./Footer.css";
+
+function Footer() {
+  return (
+    <div className="footer">
+      <p>Gradient Generator for Linear and Radial Css Color Gradients</p>
+      <p>
+        © 2022 CSS-GRADIENT.COM
+        <br />
+        <a
+          href="https://www.css-gradient.com/legal"
+          target="_blank"
+          rel="noreferrer"
+        >
+          Terms & Privacy
+        </a>
+        ~
+        <a
+          target="_blank"
+          href="mailto:hello@css-gradient.com"
+          rel="noreferrer"
+        >
+          Contact
+        </a>
+      </p>
+    </div>
+  );
+}
+
+export default Footer;

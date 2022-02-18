d343fdcddf1592a1eb0dca355aef8143857c9c40
bruno-dossantos <contactobrunods@gmail.com>
2022-02-17 23:14:46 -0300

    comments deleted

--- src/components/Button/Button.jsx
+++ src/components/Button/Button.jsx
@@ -1,16 +1,9 @@
-/* import { useState } from "react";
- */import "./Button.css";
+import "./Button.css";
 
 const Button = (props) => {
-/*   const [selected, setSelected] = useState(false);
-  function handleClick() {
-    setSelected(!selected);
-  } */
   return (
     <button
       {...props}
-/*       onClick={handleClick}
-      id={selected ? "selected" : ""} */
       className="btn"
     ></button>
   );
